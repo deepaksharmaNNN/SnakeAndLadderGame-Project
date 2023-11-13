144be9112066613c39c9e3a0e43b0ce716bbce0e
Deepak Sharma <dsharma2828@gmail.com>
2023-11-14 02:28:38 +0530

    Added Buttons

--- src/main/java/com/example/snakeandladder/SnakeLadder.java
+++ src/main/java/com/example/snakeandladder/SnakeLadder.java
@@ -2,6 +2,8 @@ package com.example.snakeandladder;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -14,9 +16,10 @@ import java.io.IOException;
 
 public class SnakeLadder extends Application {
     public static final int tileSize = 40 , height = 10 , width = 10;
+    public static final int buttonLine = height*tileSize + 50 , infoLine = buttonLine - 30;
     private Pane createContent(){
         Pane root = new Pane();
-        root.setPrefSize(width*tileSize,height*tileSize + 50);
+        root.setPrefSize(width*tileSize,height*tileSize + 100);
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -39,6 +42,37 @@ public class SnakeLadder extends Application {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //Buttons
+        Button playerOneButton = new Button("Player One");
+        Button playerTwoButton = new Button("Player Two");
+        Button startButton = new Button("Start");
+
+        playerOneButton.setTranslateY(buttonLine);
+        playerOneButton.setTranslateX(20);
+
+        playerTwoButton.setTranslateY(buttonLine);
+        playerTwoButton.setTranslateX(300);
+
+        startButton.setTranslateY(buttonLine);
+        startButton.setTranslateX(170);
+
+        //info Display
+        Label playerOneLabel = new Label("Your Turn !");
+        Label playerTwoLabel = new Label("Your Turn !");
+        Label diceLabel = new Label("Start The Game !");
+
+        playerOneLabel.setTranslateY(infoLine);
+        playerOneLabel.setTranslateX(20);
+
+        playerTwoLabel.setTranslateY(infoLine);
+        playerTwoLabel.setTranslateX(300);
+
+        diceLabel.setTranslateY(infoLine);
+        diceLabel.setTranslateX(150);
+
+        root.getChildren().addAll(playerOneButton,playerTwoButton,startButton,
+                playerOneLabel,playerTwoLabel,diceLabel
+        );
         return root;
     }
     @Override
